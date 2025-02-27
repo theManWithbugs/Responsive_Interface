85757a9b20af2e300356e07709565d9baece6e78
Wiliam de Araujo Luna Junior <wiliam.luna@iapen.ac.gov.br>
2025-02-27 12:34:40 -0500

    Input box finalizada

--- my_navbar/sidebar.js
+++ my_navbar/sidebar.js
@@ -7,6 +7,8 @@ function hidenNavBar() {
     element.classList.remove('animate__animated', 'animate__fadeInLeft');
     element.classList.add('animate__animated', 'animate__fadeOutLeft', 'animate__fast');
 
+    content.style.marginLeft = "0";
+
    setTimeout(() => {
         element.style.display = "none"; 
     }, 180);
@@ -16,7 +18,9 @@ function hidenNavBar() {
 
 function showNavBar() {
     element.style.display = "block";
-    content.style.width = "84%";
+    content.style.marginLeft = "10%"
+    content.style.marginTop= "-550px";
+    // content.style.width = "84%";
 
     element.classList.remove('animate__animated', 'animate__fadeOutLeft');
     element.classList.add('animate__animated', 'animate__fadeInLeft', 'animate__fast');
